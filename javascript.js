516e9c16a160b4b92a69b3ffc0ddca4ca1c58281
ElwDimension <ElwDimension@gmail.com>
2024-04-05 15:30:32 -0400

    Added very basic elements to html, started working on javascript to create and display objects from dialog form input

--- javascript.js
+++ javascript.js
@@ -8,5 +8,39 @@ function Book(title,author,pages,read) {
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  /*myLibrary.forEach(element => {
+    var div=document.createElement('div');
+    div.className='book';
+    div.innerHTML=`<p>boop</p>`;
+    document.getElementById('#books').appendChild(div);
+  }); */
+
+
 }
+
+const dialog = document.querySelector("dialog");
+const addButton = document.querySelector('#addBook');
+const closeButton=document.querySelector('#close');
+
+addButton.addEventListener('click',()=>{dialog.showModal();});
+closeButton.addEventListener('click', ()=>{dialog.close();})
+
+//document.querySelector('form').addEventListener('submit', (e) => {
+//  e.preventDefault();
+//});
+
+var form = document.querySelector("form");
+    form.onsubmit = function(){
+    	
+    	let newtitle = document.getElementById('title').value;
+    	/* newauthor =  document.getElementById("author").value;
+    	newpages =  document.getElementById("pages").value;
+    	newread = document.querySelector("input[name='read']:checked").value;
+
+    	newBook = new Book(newtitle,newauthor,newpages,newread);
+    	myLibrary.push(newBook); 
+      addBookToLibrary(); */
+      alert `newtitle`;
+    }
+
+
